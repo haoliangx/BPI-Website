cf5e125c2ab169d3a6567a189e4e4fd69a011e9a
Haoliang Wang <haoliangx@users.noreply.github.com>
2017-01-30 15:51:40 -0500

    Update style

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	/* Visibility toggle */
 	$(".menu-title ~ .menu-content").hide();
 	$(".menu-title").click(function(e){
-		$(this).siblings(".menu-content").toggle();
+		$(this).siblings(".menu-content").slideToggle();
 		if($(this).attr('href').indexOf('#') > -1){
 			e.preventDefault();
 		}
